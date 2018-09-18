5e66108f52bb470685b18a08ae133b1a903bbf64
Aldís Geirdal <aldisgeirdal@gmail.com>
2018-09-18 15:25:03 +0000

    program 1 done

--- Implementation_2.py
+++ Implementation_2.py
@@ -1,17 +1,3 @@
-# Algorithm for Tile Traveller:
-# 1. There are 9 tiles, three rows and three columns. 
-#       Some of the walls of the tiles have openings, but not all.
-#       The one marked 1,1 is at the left bottom.
-#       The one above is marked 1,2.
-#       The one marked 3,1 is at the right bottom. That is the vicctory location.
-# 2. The player starts at tile 1,1.
-# 3. Player is informed of possible travel options.
-# 4. Player is asked to move.  
-# 5. If player moves to a direction not available - let player know that direction is not valid. Go back to 4.
-# 6. If player moves to a direction available, relocate player in program and go back to 3.
-# 7. If player is located in tile 3,1, player is in victory location. Inform player he is in victory location.
-
-# print("You can travel: (N)orth.")    # player is at tile 3,1
 x = 1
 y = 1
 
@@ -45,7 +31,7 @@ while True:
         elif direction in east and ((x == 1 and y == 2) or (x == 1 and y == 3) or (x == 2 and y == 3)):
             x += 1
             break
-        elif direction in south and ((x == 1 and y == 2) or (2 == 1 and y == 2) or (x == 3 and y == 3) or (x == 3 and y == 2) or (x == 2 and y == 2)):
+        elif direction in south and ((x == 1 and y == 2) or (2 == 1 and y == 2) or (x == 3 and y == 3) or (x == 3 and y == 2)):
             y -= 1
             break
         elif direction in west and ((x == 2 and y == 3) or (x == 2 and y == 2) or (x == 3 and y == 3)):
