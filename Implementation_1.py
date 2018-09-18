a5a1a398437b2970b35ddd61926a0bc6035e2c51
Aldís Geirdal <aldisgeirdal@gmail.com>
2018-09-18 12:51:12 +0000

    git commit -m "Algorithm"

---
+++ Implementation_1.py
@@ -0,0 +1,8 @@
+# Algorithm
+# 1. The player starts at tile 1,1.
+# 2. Inform player of possible travel options.
+# 3. Ask player to move.  
+# 4. If player moves to a direction not available - let player know that direction is not valid. Go back to 3.
+# 5. If player moves to a direction available, relocate player in program and go back to 2.
+# 6. If player is located in tile 3,1 - on the bottom right of the tile, player is in victory location.
+# inform player he is in victory location.
