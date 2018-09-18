7b7e9fcf88d649dda09781b78237443bc0848967
Aldís Geirdal <aldisgeirdal@gmail.com>
2018-09-18 13:05:34 +0000

    Algorithm improved

--- Implementation_1.py
+++ Implementation_1.py
@@ -1,8 +1,13 @@
-# Algorithm
-# 1. The player starts at tile 1,1.
-# 2. Inform player of possible travel options.
-# 3. Ask player to move.  
-# 4. If player moves to a direction not available - let player know that direction is not valid. Go back to 3.
-# 5. If player moves to a direction available, relocate player in program and go back to 2.
-# 6. If player is located in tile 3,1 - on the bottom right of the tile, player is in victory location.
-# inform player he is in victory location.
+# Algorithm for Tile Traveller:
+# 1. There are 9 tiles, three rows and three columns. 
+#       Some of the walls of the tiles have openings, but not all.
+#       The one marked 1,1 is at the left bottom.
+#       The one above is marked 1,2.
+#       The one marked 3,1 is at the right bottom. That is the vicctory location.
+# 2. The player starts at tile 1,1.
+# 3. Player is informed of possible travel options.
+# 4. Player is asked to move.  
+# 5. If player moves to a direction not available - let player know that direction is not valid. Go back to 4.
+# 6. If player moves to a direction available, relocate player in program and go back to 3.
+# 7. If player is located in tile 3,1, player is in victory location. Inform player he is in victory location.
+
